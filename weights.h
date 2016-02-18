98efc08869a27c5590d4a3ea7606b81ff68c3a5a
Anderson <7053313@linux11.mcs.sdsmt.edu>
2016-02-17 17:15:19 -0700

    really

--- weights.h
+++ weights.h
@@ -9,7 +9,7 @@ using namespace std;
 
 bool ReadWeightsFromFile(string weightsFile, double* &weightArray);
 
-bool WriteWeightsToFile(string weightsFIle, double* &weightArray, vector<int> inOut);
+bool WriteWeightsToFile(string weightsFIle, double* &weightArray, vector<int> nodesPerLayer);
 
 void CreateWeights(string weightsFileName, double* &weightArray, vector<int> nodesPerLayer);
 
